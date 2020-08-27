605ff7f772e75220f272113e42e3d7a8004547b2
朱飞 <1147786105@qq.com>
2020-08-27 16:57:18 +0800

    Create self.vue

---
+++ src/components/actives/self.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="self">
+        <img src="../../assets/img/home4.png">
+        <img src="../../assets/img/self/web_selfservice_03.png">
+        <img src="../../assets/img/self/web_selfservice_04.png">
+        <img src="../../assets/img/self/web_selfservice_05.png">
+        <img src="../../assets/img/self/web_selfservice_06.png">
+        <img src="../../assets/img/self/web_selfservice_07.png">
+        <img src="../../assets/img/self/web_selfservice_08.png">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "self",
+    }
+</script>
+
+<style scoped>
+    .self img{
+        width: 100%;
+        vertical-align: middle;
+    }
+</style>
