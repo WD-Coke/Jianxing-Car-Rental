d9cabcd007f7c0e52b39beae535f318649cc86a2
朱飞 <1147786105@qq.com>
2020-08-27 16:50:03 +0800

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <tap-bar></tap-bar>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  import TapBar from "./components/tap-bar/TapBar";
+  export default {
+    components:{
+      TapBar
+
+    }
+  }
+</script>
+<style>
+body{
+  margin: 0;
+  padding: 0;
+  font-size: 62.5%;
+}
+  a{
+    text-decoration: none;
+  }
+  ul{
+    margin: 0;
+    padding: 0;
+  }
+  li{
+    list-style-type: none;
+  }
+</style>
