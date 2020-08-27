29201ee61913e94dbf8642daf32d0979c23c102d
朱飞 <1147786105@qq.com>
2020-08-27 16:57:09 +0800

    Create driving.vue

---
+++ src/components/actives/driving.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="self-driving">
+        <img src="../../assets/img/home1.png">
+        <img src="../../assets/img/driving/pcjuemei_04.jpg">
+        <img src="../../assets/img/driving/pcjuemei_05.jpg">
+        <img src="../../assets/img/driving/pcjuemei_06.jpg">
+        <img src="../../assets/img/driving/pcjuemei_07.jpg">
+        <img src="../../assets/img/driving/pcjuemei_08.jpg">
+        <img src="../../assets/img/driving/pcjuemei_09.jpg">
+        <img src="../../assets/img/driving/pcjuemei_10.jpg">
+        <img src="../../assets/img/driving/pcjuemei_11.jpg">
+        <img src="../../assets/img/driving/pcjuemei_12.jpg">
+        <img src="../../assets/img/driving/pcjuemei_13.jpg">
+        <img src="../../assets/img/driving/pcjuemei_14.jpg">
+        <img src="../../assets/img/driving/pcjuemei_15.jpg">
+        <img src="../../assets/img/driving/pcjuemei_16.jpg">
+        <img src="../../assets/img/driving/pcjuemei_17.jpg">
+        <img src="../../assets/img/driving/pcjuemei_18.jpg">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "driving",
+        components: {
+        }
+    }
+</script>
+
+<style scoped>
+    .self-driving img{
+        width: 100%;
+        vertical-align: middle;
+    }
+</style>
