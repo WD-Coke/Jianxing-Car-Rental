5a1fbce37aacf19eef64a4ab113f68c46d033f26
朱飞 <1147786105@qq.com>
2020-08-27 16:57:20 +0800

    Create ski.vue

---
+++ src/components/actives/ski.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="ski">
+        <img src="../../assets/img/home2.png">
+        <div class="pic-list">
+            <img src="../../assets/img/ski/list_01.jpg">
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ski",
+        components:{
+
+        }
+    }
+</script>
+
+<style scoped>
+    .ski img{
+        width: 100%;
+        vertical-align: middle;
+    }
+    .pic-list{
+        width: 70%;
+        margin: auto;
+        padding: 5% 0 5% 0;
+    }
+</style>
