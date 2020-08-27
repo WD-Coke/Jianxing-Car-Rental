1fe8ac274a1e1a3f34d3d0e122591ca7d51fda1a
朱飞 <1147786105@qq.com>
2020-08-27 16:57:15 +0800

    Create free.vue

---
+++ src/components/actives/free.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="free">
+        <img src="../../assets/img/home3.png">
+        <img src="../../assets/img/free/web_duanwu2020_04.png">
+        <img src="../../assets/img/free/web_duanwu2020_05.png">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "free",
+        components:{
+
+        }
+    }
+</script>
+
+<style scoped>
+    .free img{
+        width: 100%;
+        vertical-align: middle;
+    }
+</style>
